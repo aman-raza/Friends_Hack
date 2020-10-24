90a0ad6dd1559bc42cf2fb24131361ac138eba10
sristi sharma <54236156+24sristi@users.noreply.github.com>
2020-10-25 01:59:28 +0530

    Create countOfDigitsInaNumber.cpp
    Given an integral number N. The task is to find the count of digits present in this number within O(1) time complexity.

---
+++ countOfDigitsInaNumber.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int k = log10(n)+1;
+    cout<<k;
+}
+//following method only requires O(1) time complexity
