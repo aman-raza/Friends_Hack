bb3dda3f5c3e0c22bf327e18f32a5858d1665a26
Kavya Agrawal <kavya.agrawal162@gmail.com>
2021-10-27 12:08:48 +0530

    Added Insertion Sort Code in C

---
+++ Insertion Sort.java
@@ -0,0 +1,32 @@
+//Insertion Sort Code in C
+
+#include<stdio.h>
+int main()
+{
+
+     int i, j, count, temp, number[25];
+
+   printf("How many numbers u wish to enter?: ");
+   scanf("%d",&count);
+
+   printf("Enter %d elements: ", count);
+   for(i=0;i<count;i++)
+      scanf("%d",&number[i]);
+
+   // Implementation of insertion sort algorithm
+   for(i=1;i<count;i++){
+      temp=number[i];
+      j=i-1;
+      while((temp<number[j])&&(j>=0)){
+         number[j+1]=number[j];
+         j=j-1;
+      }
+      number[j+1]=temp;
+   }
+
+   printf("Order of Sorted elements: ");
+   for(i=0;i<count;i++)
+      printf(" %d",number[i]);
+
+   return 0;
+}
